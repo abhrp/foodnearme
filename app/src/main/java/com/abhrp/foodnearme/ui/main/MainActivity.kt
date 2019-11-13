014c2e4da6028c50d55401105be0d7f68350a9d3
abhrp <abhiroop.patel@gmail.com>
2019-11-13 16:16:27 +0530

    Refactored post testing the map api

--- app/src/main/java/com/abhrp/foodnearme/ui/main/MainActivity.kt
+++ app/src/main/java/com/abhrp/foodnearme/ui/main/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.abhrp.foodnearme.ui.main
 
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import com.abhrp.foodnearme.R
+import com.abhrp.foodnearme.ui.base.BaseActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : BaseActivity(), OnMapReadyCallback {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,23 +18,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-//    override fun online() {
-//
-//    }
-//
-//    override fun offline() {
-//
-//    }
+    override fun online() {
+
+    }
+
+    override fun offline() {
+
+    }
 
     override fun onMapReady(map: GoogleMap?) {
-        map?.addMarker(
-            MarkerOptions().position(
-                LatLng(
-                    0.0,
-                    0.0
-                )
-            ).title("Marker")
-        )
-        Log.i(MainActivity::class.java.canonicalName, "Map ready")
     }
 }
