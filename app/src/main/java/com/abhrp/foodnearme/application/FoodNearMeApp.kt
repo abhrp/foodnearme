230b4f3ef0940cc559d38a1ab33dfa8ece7c927f
abhrp <abhiroop.patel@gmail.com>
2019-11-13 13:04:25 +0530

    BaseActivity added

--- app/src/main/java/com/abhrp/foodnearme/application/FoodNearMeApp.kt
+++ app/src/main/java/com/abhrp/foodnearme/application/FoodNearMeApp.kt
@@ -1,6 +1,16 @@
 package com.abhrp.foodnearme.application
 
 import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
-class FoodNearMeApp: Application() {
+class FoodNearMeApp: DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
--- app/src/main/java/com/abhrp/foodnearme/ui/main/MainActivity.kt
+++ app/src/main/java/com/abhrp/foodnearme/ui/main/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.abhrp.foodnearme.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abhrp.foodnearme.R
+import com.abhrp.foodnearme.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun online() {
+
+    }
+
+    override fun offline() {
+
     }
 }
